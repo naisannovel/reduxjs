e410c6b3754896c271e48794934219dd46aa61be
naisannovel <naisannovel@gmail.com>
2021-09-02 00:58:40 +0600

    update

--- store.js
+++ store.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import { reducer } from "./reducer";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
-const myStore = createStore(reducer,applyMiddleware(thunk));
+const myStore = createStore(reducer, applyMiddleware(thunk));
 
 export default myStore;
+
+// jodi kono form data UI te show korar dorkar na pore kintu form data ta just server a store korle e hobe
+// se khetre se component ar moddhe axios call kore post kore dile e hobe. redux ar sathe connect korar dorkar nai
